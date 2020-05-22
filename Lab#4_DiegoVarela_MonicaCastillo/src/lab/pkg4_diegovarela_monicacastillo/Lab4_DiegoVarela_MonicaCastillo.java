534b1e3f4a5093450d5fbba4281cc6a716a60e4e
monicastle <monicastillo@unitec.edu>
2020-05-22 14:20:56 -0600

    Registro y Log In Terminadas

--- Lab#4_DiegoVarela_MonicaCastillo/src/lab/pkg4_diegovarela_monicacastillo/Lab4_DiegoVarela_MonicaCastillo.java
+++ Lab#4_DiegoVarela_MonicaCastillo/src/lab/pkg4_diegovarela_monicacastillo/Lab4_DiegoVarela_MonicaCastillo.java
@@ -16,6 +16,9 @@ public class Lab4_DiegoVarela_MonicaCastillo {
 
     static Scanner entrada = new Scanner(System.in);
     static ArrayList<String> usernames = new ArrayList();
+    static ArrayList<String> contraseñas = new ArrayList();
+    static ArrayList ingenieros = new ArrayList();
+    static ArrayList transportes = new ArrayList();
 
     /**
      * @param args the command line arguments
@@ -24,7 +27,6 @@ public class Lab4_DiegoVarela_MonicaCastillo {
         // TODO code application logic here
         char resp = 's';
         while (resp == 's' || resp == 'S') {
-
             System.out.println("***MENU***");
             System.out.println("1) Registro");
             System.out.println("2) Log In");
@@ -34,6 +36,8 @@ public class Lab4_DiegoVarela_MonicaCastillo {
             switch (opcion) {
                 case 1:
                     System.out.println("¡Bienvenido al Sistema de Registro!");
+                    System.out.print("Ingrese su nombre: ");
+                    String nombre = entrada.nextLine();
                     System.out.print("Ingrese su correo electronico: ");
                     String correo = entrada.next();
                     System.out.print("Ingrese un nombre de usuario: ");
@@ -43,6 +47,7 @@ public class Lab4_DiegoVarela_MonicaCastillo {
                         System.out.print("Ingrese un nombre de usuario: ");
                         usuario = entrada.next();
                     } // Fin While
+                    usernames.add(usuario);
                     ArrayList<String> idiomas = new ArrayList();
                     char resp2 = 's';
                     while (resp2 == 's' || resp2 == 'S') {
@@ -88,11 +93,56 @@ public class Lab4_DiegoVarela_MonicaCastillo {
                     } // Fin While Respuesta
                     System.out.print("Ingrese su contraseña: ");
                     String contraseña = entrada.next();
+                    contraseñas.add(contraseña);
                     System.out.print("Ingrese su fecha de nacimiento :");
                     String fecha = entrada.next();
+                    System.out.print("Ingrese su grupo sanguineo :");
+                    String sanguineo = entrada.next();
+                    System.out.println("***Genero***");
+                    System.out.println("1. Femenino");
+                    System.out.println("2. Masculino");
+                    System.out.println("Ingrese su genero: ");
+                    int opcion2 = entrada.nextInt();
+                    String sexo = "";
+                    switch (opcion2) {
+                        case 1:
+                            sexo = "Femenino";
+                            break;
+                        case 2:
+                            sexo = "Masculino";
+                            break;
+                        default:
+                            System.out.println("¡Entrada no valida!");
+                    } // Fin Switch Generos
+                    System.out.print("Ingrese su altura: ");
+                    double altura = entrada.nextDouble();
+                    System.out.println("Ingrese su peso: ");
+                    double peso = entrada.nextDouble();
                     System.out.println("¡Se ha logrado registrar exitosamente!");
+                    ingenieros.add(new Ingenieros(correo, usuario, contraseña, fecha, nombre, sanguineo, sexo, altura, peso));
                     break;
                 case 2:
+                    System.out.print("Ingrese su nombre de uuario: ");
+                    String user2 = entrada.next();
+                    while (Usernames(user2) == false) {
+                        System.out.println("¡El username ingresado no se encuentra registrado!");
+                        System.out.print("Ingrese su nombre de usuario: ");
+                        user2 = entrada.next();
+                    } // Fin While
+                    System.out.print("Ingrese su contraseña: ");
+                    String contra2 = entrada.next();
+                    while (Registro(user2, contra2) == false) {
+                        System.out.println("¡Contraseña no valida!");
+                        System.out.print("Ingrese su contraseña: ");
+                        contra2 = entrada.next();
+                    } // Fin While
+                    System.out.println("¡Bienvenido " + user2 + "!");
+                    System.out.println("¿Que desea hacer el dia de hoy?");
+                    System.out.println("***MENU***");
+                    System.out.println("1) Agregar Ingeniero");
+                    System.out.println("2) Modificar Ingeniero");
+                    System.out.println("3) Eliminar Ingeniero");
+                    System.out.println("4) Viajar");
                     break;
                 case 3:
                     resp = 'n';
@@ -112,22 +162,22 @@ public class Lab4_DiegoVarela_MonicaCastillo {
             } // Fin If
         } // Fin For
         return false;
-    } // Fin Codigos
+    } // Fin Usernames
 
-//    System.out.println (
-//
-//    "***MENU***");
-//    System.out.println (
-//
-//    "1) Agregar Ingeniero");
-//    System.out.println (
-//
-//    "2) Modificar Ingeniero");
-//    System.out.println (
-//
-//    "3) Eliminar Ingeniero");
-//    System.out.println (
-//
-//"4) Viajar");
+    public static boolean Registro(String username, String password) {
+        int pos;
+        for (int i = 0; i < usernames.size(); i++) {
+            if (usernames.get(i).equals(username)) {
+                pos = i;
+                if (contraseñas.get(pos).equals(password)) {
+                    return true;
+                } else {
+                    return false;
+                }// Fin If
+            } // Fin If
+        } // Fin For
+        return false;
+    } // Fin Registro
+
 } // Fin Class
 
